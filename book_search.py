f2d596f651f7b1b805c4064800ce316ad3cc3aa5
best33gh <83940722+best33gh@users.noreply.github.com>
2021-05-19 01:38:22 +0900

    book_search.py is updated by student2!

--- book_search.py
+++ book_search.py
@@ -1,6 +1,17 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import matplotlib.pyplot as plt
+
+import matplotlib.font_manager as fm
+font_fname = 'C:/Windows/Fonts/NanumGothic.ttf'
+font_family = fm.FontProperties(fname=font_fname).get_name()
+plt.rcParams["font.family"] = font_family
+plt.rcParams['font.size'] = 15.
+plt.rcParams['xtick.labelsize'] = 6.
+plt.rcParams['ytick.labelsize'] = 6.
+plt.rcParams['axes.labelsize'] = 10.
+
 
 print("가격이 궁금한 책의 제목을 3개 입력하시오(콤마로 구분할 것)")
 print('ex: 이기적 유전자,사피엔스,아몬드')
@@ -11,6 +22,8 @@ URL= 'http://www.yes24.com/main/default.aspx?ysmchn=ggl&ysmcpm=google-sponsor&ys
 driver=webdriver.Chrome()
 driver.get(url=URL)
 author_list=[]
+book_price=[]
+
 
 for book in book_list:
     driver.find_element_by_css_selector('#query.iptTxt').send_keys(book)
@@ -20,6 +33,9 @@ for book in book_list:
     author = driver.find_element_by_class_name('gd_auth').text
     author_list.append(author)
 
+    price=driver.find_element_by_class_name('nor_price').text
+    book_price.append(price)
+
     time.sleep(3)
 
 book_dic=dict()
@@ -28,3 +44,28 @@ for i in range(3):
 print(book_dic)
 for title,author in book_dic.items():
     print(title,'-',author)
+
+book__price=[]
+
+
+for i in book_price:
+    i=list(i)
+    if "," in i:
+        i.remove(",")
+    if "원" in i:
+        i.remove("원")
+    i = str(i)
+    i = i[2] + i[7] + i[12] + i[17]
+    i=int(i)
+    book__price.append(i)
+
+
+book__price=list(book__price)
+
+x=[1,2,3]
+title=book_list
+
+plt.bar(x,book__price)
+plt.suptitle('책들의 가격')
+plt.xticks(x,title)
+plt.show()
